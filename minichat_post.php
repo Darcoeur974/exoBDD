2f5b865d57c8d89e6a041b3c63249e76d38337cc
Darcoeur974 <darcoeur136dark@gmail.com>
2019-10-16 13:27:23 +0400

    etape final

--- minichat_post.php
+++ minichat_post.php
@@ -7,17 +7,15 @@ catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
 }
-echo $_POST['login'];
-echo $_POST['message'];
-//$_POST['login'] = $login;
-//$_POST['message'] = $message;
 
-/*
+$login = $_POST['login'];
+$message = $_POST['message'];
+
 $req = $bdd->prepare('INSERT INTO utilisateur(login, message) VALUES(:login, :message)');
 $req->execute(array(
     'login' => $login,
     'message' => $message
 ));
-*/
-//header('Location: minichat.php');
+
+header('Location: minichat.php');
 ?>
